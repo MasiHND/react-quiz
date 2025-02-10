43879d10544a349e13e1f39d7cfbcef949c999ce
MasiHND <pouya.hendy@gmail.com>
2025-02-10 18:13:54 +0330

    starting project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import DateCounter from "./DateCounter";
+
+function App() {
+  return (
+    <div>
+      <DateCounter />
+    </div>
+  );
+}
+
+export default App;
